902a0dc89c5a539914f8e41469b4bfed4ec9cd49
Christian Shewmake <cshewmake2@gmail.com>
2021-02-23 16:51:17 -0600

    Add experiment template.

--- experiment.py
+++ experiment.py
@@ -1,161 +1,105 @@
-from collections import OrderedDict
 import datetime
 import os
 
-from pytorch_metric_learning import losses, distances
-from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-# from torch.optim import Adam
- 
-class Experiment(torch.nn.Module): # should we make this inherit Module?
-    def __init__(self, experiment_name, task_name, dataset_name, model, dataset, optimizer, optimizer_params, regularizers, regularizer_params, device = 'cuda'):
+
+class Experiment(torch.nn.Module):
+    def __init__(
+        self,
+        experiment_name,
+        model,
+        optimizer,
+        optimizer_params,
+        regularizer,
+        regularizer_params,
+        device="cuda",
+    ):
         self.experiment_name = experiment_name
-        self.task_name = task_name
-        self.dataset_name = dataset_name
         self.model = model
-        self.dataset = dataset
         self.optimizer = optimizer
         self.optimizer_params = optimizer_params
-        self.regularizers = regularizers
+        self.regularizer = regularizer
         self.regularizer_params = regularizer_params
         self.device = device
-        self.writer = None  # pass this in or create it inside?
-        try:
-            self.logdir = create_logdir()
-        except:
-            raise Exception("Problem creating logging and/or checkpoint directory.")
-    
+        self.logdir = None
+        self.dataset_name = None
+
+    # TODO: what should be part of the path?
     def create_logdir(self):
-        logdir = os.path.join('logs', 
-                              self.experiment_name, 
-                              self.model.__class__.__name__,
-                              self.dataset_name)
-        
+        logdir = os.path.join(
+            "logs",
+            self.experiment_name,
+            self.model.__class__.__name__,
+            self.dataset_name,
+        )
+
         os.makedirs(logdir, exist_ok=True)
 
         logdir = os.path.join(logdir, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
         os.mkdir(logdir)
-        os.mkdir(os.path.join(logdir, 'checkpoints'))
+        os.mkdir(os.path.join(logdir, "checkpoints"))
         return logdir
 
-    def log(self, epoch, training_loss, validation_loss):
-        print('Epoch: {} || Training L: {:.5f} | Validation Loss: {:.5f}'.format(epoch, training_loss, validation_loss))
+    # TODO: what else should we log?
+    # Should we give options or leave that to a subclass?
+    def save_checkpoint(self):
+        if epoch % 10 == 0:
+            torch.save(
+                {
+                    "epoch": epoch,
+                    "model_state_dict": self.model.state_dict(),
+                    "optimizer_state_dict": self.optimizer.state_dict(),
+                },
+                os.path.join(
+                    self.logdir, "checkpoints", "checkpoint_{}.pt".format(epoch)
+                ),
+            )
 
-        self.writer.add_scalar('loss/train', training_loss, epoch)
-        self.writer.add_scalar('loss/val', validation_loss, epoch)
-        # what else?
+    # logstring = "Epoch: {} || Training L: {:.5f}".format(epoch, variable)
+    def log(self, epoch, group, name, value):
+        self.writer.add_scalar("{}/{}".format(group, name), value, epoch)
 
-        if epoch % 10 == 0: #make the interval a parameter or a function of the total num_epochs?
-            torch.save({
-                        'epoch': epoch,
-                        'model_state_dict': self.model.state_dict(),
-                        'optimizer_state_dict': self.optimizer.state_dict()}, 
-                        os.path.join(self.logdir, 'checkpoints', 'checkpoint_{}.pt'.format(epoch)))
-    
     # right now this is an overridden method by the inherited class...
     # do we want models to inherit experiment or for experiments to ingest models?
     #   ingest? -> 1) model provides its own train step, this calls it
     #              2) New experiment subclass for each train step type (meh), idk
     #              3) new class
-    def train_step(self,
-                   data,
-                   grad=True,
-                   output=False):
-        
+    def train_step(self, data, grad=True, output=False):
+
         raise NotImplementedError
 
     # do we want to pass in data and let the train loading scheme be passed in/parameterized in constructor?
-    def train(self, 
-            train_loader, 
-            validation_loader,
-            epochs,
-            start_epoch=0):
+    def train(self, data_loader, epochs, start_epoch=0):
+        try:
+            self.dataset_name = data_loader.name
+            self.logdir = self.create_logdir()
+            self.writer = SummaryWriter(self.logdir)
+        except:
+            raise Exception("Problem creating logging and/or checkpoint directory.")
 
         for i in range(start_epoch, epochs):
-            training_loss = self.train_step(train_loader, grad=True)
-            validation_loss = self.train_step(validation_loader, grad=False)
-            self.log(i, training_loss, validation_loss)
+            training_loss = self.train_step(data_loader.train, grad=True)
+            self.log(i, group="loss", name="train", value=training_loss)
 
+            if data_loader.val is not None:
+                validation_loss = self.train_step(data_loader.val, grad=False)
+                self.log(i, group="loss", name="val", value=validation_loss)
 
-#this depends implicitly on self.model, hdim being set by a child
-class Model(torch.nn.Module):
-    
-    def __init__(self):
-        super().__init__()
-        self.field = None
-        self.name = None
-        self.writer = None
-        
-    def create_logdir(self):
-        
-        logdir = os.path.join('logs', 
-                              self.experiment_name, 
-                              self.dataset,
-                              self.name, 
-                              self.field, 
-                              str(self.hdim))
-        
-        os.makedirs(logdir, exist_ok=True)
+    def load_checkpoint(self, path, checkpoint):
+        state_dict = torch.load(
+            path + "checkpoints/checkpoint_{}.pt".format(checkpoint)
+        )
+        self.model.load_state_dict(state_dict["model_state_dict"])
+        self.optimizer.load_state_dict(state_dict["optimizer_state_dict"])
 
-        logdir = os.path.join(logdir, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-
-        os.mkdir(logdir)
-        os.mkdir(os.path.join(logdir, 'checkpoints'))
-        self.logdir = logdir
-        
-    def log(self, epoch, training_loss, validation_loss):
-        print('Epoch: {} || Training L: {:.5f} | Validation Loss: {:.5f}'.format(epoch, training_loss, validation_loss))
-
-        self.writer.add_scalar('loss/train', training_loss, epoch)
-        self.writer.add_scalar('loss/val', validation_loss, epoch)
-
-        if epoch % 10 == 0:
-            torch.save({
-                        'epoch': epoch,
-                        'model_state_dict': self.model.state_dict(),
-                        'optimizer_state_dict': self.optimizer.state_dict()}, 
-                        os.path.join(self.logdir, 'checkpoints', 'checkpoint_{}.pt'.format(epoch)))
-            
-    def train_step(self,
-                   data,
-                   grad=True,
-                   output=False):
-        
-        raise NotImplementedError
-            
-    def train(self, 
-              train_loader, 
-              validation_loader,
-              epochs,
-              start_epoch=0):
-
-        for i in range(start_epoch, epochs):
-            training_loss = self.train_step(train_loader, grad=True)
-            validation_loss = self.train_step(validation_loader, grad=False)
-            self.log(i, training_loss, validation_loss)
-            
-    def load_checkpoint(self,
-                        path, 
-                        checkpoint):
-        state_dict = torch.load(path+'checkpoints/checkpoint_{}.pt'.format(checkpoint))
-        self.model.load_state_dict(state_dict['model_state_dict'])
-        self.optimizer.load_state_dict(state_dict['optimizer_state_dict'])
-        
-    def resume(self,
-               path,
-               checkpoint,
-               train_loader,
-               validation_loader, 
-               epochs):
+    def resume(self, path, checkpoint, data_loader, epochs):
         self.load_checkpoint(path, checkpoint)
-        self.train(train_loader, validation_loader, epochs, start_epoch=checkpoint)
+        self.train(data_loader, epochs, start_epoch=checkpoint)
 
-    def eval(self,
-             test_loader,
-             output=True):
+    ## this may not work if training has access to things not available in test
+    def evaluate(self, test_loader, output=True):
         out = self.train_step(test_loader, grad=False, output=True)
-        return out        
-            
+        return out
