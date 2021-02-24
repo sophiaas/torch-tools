f1432dd77f11a9c09914e55c03e1b64303dfdcd3
Sophia Sanborn <sophia@Sophias-MacBook-Pro.local>
2021-02-23 16:57:45 -0800

    fuller template

--- examples/template.py
+++ examples/template.py
@@ -8,7 +8,7 @@ from torch.optim import Adam
 from torch_tools.functional import l1_norm, matrix_2_norm
 
 
-model_params = ParameterDict({})
+model_params = ParameterDict({"n_neurons": 64})
 
 data_params = ParameterDict(
     {
@@ -22,14 +22,63 @@ data_params = ParameterDict(
 )
 
 
-optimizer_params = ParameterDict(
-    {
-        "lr": 1e-3,
-    }
-)
+optimizer_params = ParameterDict({"lr": 1e-3})
 
 optimizer = Adam(**optimizer_params)
 
 reg1 = Regularizer(l1_norm, "s", 0.7)
 reg2 = Regularizer(matrix_2_norm, "A", 0.1)
 regularizer = MultiRegularizer([reg1, reg2])
+
+
+class TestExperiment(Experiment):
+    def __init__(
+        self,
+        experiment_name,
+        model,
+        optimizer,
+        regularizer,
+        loss_function,
+        device="cuda",
+    ):
+        super().__init__(
+            self, experiment_name, model, optimizer, regularizer, device="cuda"
+        )
+        self.loss_function = loss_function
+
+    def train_step(self, data_loader, grad=True):
+
+        total_L = 0
+
+        for i, (x, labels) in enumerate(data):
+            x = x.to(self.device)
+            labels = labels.to(self.device)
+
+            if grad:
+                self.optimizer.zero_grad()
+                y = self.forward(x)
+            else:
+                with torch.no_grad():
+                    y = self.forward(x)
+
+            L = self.loss_function(y, labels)
+
+            if grad:
+                L.backward()
+                self.optimizer.step()
+
+            total_L += L
+
+        total_L /= len(data)
+
+        return total_L
+
+
+model = torch.nn.Sequential([torch.nn.Linear(model_params.n_neurons)])
+
+experiment_name = "test"
+loss = torch.nn.CrossEntropyLoss()
+
+experiment = TestExperiment(experiment_name, model, optimizer, regularizer, loss)
+
+experiment.train()
