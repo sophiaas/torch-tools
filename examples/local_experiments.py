c614eb76b939816d79db1a94906b6debb027170d
Christian Shewmake <cshewmake2@gmail.com>
2021-02-24 22:16:25 -0600

    Log hyperparameters in tensorboard.

--- examples/local_experiments.py
+++ examples/local_experiments.py
@@ -20,8 +20,21 @@ class TestExperiment(Experiment):
         self.writer.add_graph(self.model, batch_x)
 
         train_x, train_y = data.train.dataset[data.train.batch_sampler.sampler.indices]
-        self.writer.add_graph(self.model, train_x)
         self.writer.add_embedding(train_x, metadata=train_y)
+
+    def log_on_end(self, data):
+        # log train, val, and hyperparameters
+        hparam_dict = {
+            "lr": self.optimizer.param_groups[0]["lr"],
+            "bsize": data.batch_size,
+        }
+        train_loss = self.evaluate(data.train)
+        metric_dict = {"hparam/train_loss": train_loss}
+        if data.val is not None:
+            validation_loss = self.evaluate(data.val)
+            metric_dict["hparam/val_loss"] = validation_loss
+
+        self.writer.add_hparams(hparam_dict, metric_dict)
 
     def log_model_params(self, epoch):
         self.writer.add_histogram("linear0.weights", self.model.layers[0].weight, epoch)
