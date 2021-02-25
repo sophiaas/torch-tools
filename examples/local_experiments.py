0fbeb70990d150e9d9acc34b6ac4bf3079c3b7f1
Christian Shewmake <cshewmake2@gmail.com>
2021-02-25 14:19:40 -0600

    Rearrange functions.

--- examples/local_experiments.py
+++ examples/local_experiments.py
@@ -15,31 +15,6 @@ class TestExperiment(Experiment):
         super().__init__(experiment_name, model, optimizer, regularizer, device=device)
         self.loss_function = loss_function
 
-    def log_on_start(self, data):
-        batch_x, batch_y = next(iter(data.train))
-        self.writer.add_graph(self.model, batch_x)
-
-        train_x, train_y = data.train.dataset[data.train.batch_sampler.sampler.indices]
-        self.writer.add_embedding(train_x, metadata=train_y)
-
-    def log_on_end(self, data):
-        # log train, val, and hyperparameters
-        hparam_dict = {
-            "lr": self.optimizer.param_groups[0]["lr"],
-            "bsize": data.batch_size,
-        }
-        train_loss = self.evaluate(data.train)
-        metric_dict = {"hparam/train_loss": train_loss}
-        if data.val is not None:
-            validation_loss = self.evaluate(data.val)
-            metric_dict["hparam/val_loss"] = validation_loss
-
-        self.writer.add_hparams(hparam_dict, metric_dict)
-
-    def log_model_params(self, epoch):
-        self.writer.add_histogram("linear0.weights", self.model.layers[0].weight, epoch)
-        self.writer.add_histogram("linear0.bias", self.model.layers[0].bias, epoch)
-
     def train_step(self, data, epoch=None):
         total_L = 0
         for i, (x, labels) in enumerate(data):
@@ -71,3 +46,29 @@ class TestExperiment(Experiment):
                 total_L += L
             total_L /= len(data)
         return total_L
+
+    # LOGGING
+    def log_on_start(self, data):
+        batch_x, batch_y = next(iter(data.train))
+        self.writer.add_graph(self.model, batch_x)
+
+        train_x, train_y = data.train.dataset[data.train.batch_sampler.sampler.indices]
+        self.writer.add_embedding(train_x, metadata=train_y)
+
+    def log_on_end(self, data):
+        # log train, val, and hyperparameters
+        hparam_dict = {
+            "lr": self.optimizer.param_groups[0]["lr"],
+            "bsize": data.batch_size,
+        }
+        train_loss = self.evaluate(data.train)
+        metric_dict = {"hparam/train_loss": train_loss}
+        if data.val is not None:
+            validation_loss = self.evaluate(data.val)
+            metric_dict["hparam/val_loss"] = validation_loss
+
+        self.writer.add_hparams(hparam_dict, metric_dict)
+
+    def log_model_params(self, epoch):
+        self.writer.add_histogram("linear0.weights", self.model.layers[0].weight, epoch)
+        self.writer.add_histogram("linear0.bias", self.model.layers[0].bias, epoch)
