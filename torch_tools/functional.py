47818f38bcd5deb582da5528b36b14c86083aa45
Christian Shewmake <cshewmake2@gmail.com>
2021-02-25 18:30:06 -0600

    Change from gs to torch.

--- torch_tools/functional.py
+++ torch_tools/functional.py
@@ -1,9 +1,5 @@
-import geomstats as gs
+import torch
 
 
 def l1_norm(x):
-    return gs.sum(gs.sum(gs.abs(x), axis=1))
-
-
-def matrix_2_norm(matrix):
-    return gs.sum(gs.linalg.norm(matrix, axis=(1, 2)) ** 2)
+    return torch.sum(torch.abs(x))
