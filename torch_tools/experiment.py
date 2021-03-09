ef6f535927edd3a46db2c0273c4581087d5e183a
Sophia Sanborn <sophia@Sophias-MacBook-Pro.local>
2021-03-09 09:35:21 -0800

    adds printing function and some details for logging

--- torch_tools/experiment.py
+++ torch_tools/experiment.py
@@ -1,11 +1,10 @@
 import datetime
 import os
 import pickle
-
 import torch
 from torch.utils.tensorboard import SummaryWriter
-
-import generic
+import torch_tools.generic as generic
+from tqdm import tqdm
 
 
 class Experiment(torch.nn.Module):
@@ -16,7 +15,10 @@ class Experiment(torch.nn.Module):
         optimizer,
         regularizer,
         device="cuda",
+        print_frequency=1,
+        save_frequency=10,
     ):
+        super().__init__()
         self.experiment_name = experiment_name
         self.model = model
         self.optimizer = optimizer
@@ -25,6 +27,8 @@ class Experiment(torch.nn.Module):
         self.logdir = None
         self.dataset_name = None
         self.loss_function = None
+        self.print_frequency = print_frequency
+        self.save_frequency = save_frequency
 
     # TODO: what should be part of the path?
     def create_logdir(self):
@@ -45,8 +49,8 @@ class Experiment(torch.nn.Module):
 
     # TODO: what else should we log?
     # Should we give options or leave that to a subclass?
-    def save_checkpoint(self):
-        if epoch % 10 == 0:
+    def save_checkpoint(self, epoch):
+        if epoch % self.save_frequency == 0:
             torch.save(
                 {
                     "epoch": epoch,
@@ -61,6 +65,7 @@ class Experiment(torch.nn.Module):
     # logstring = "Epoch: {} || Training L: {:.5f}".format(epoch, variable)
     def log(self, epoch, group, name, value):
         self.writer.add_scalar("{}/{}".format(group, name), value, epoch)
+        self.save_checkpoint(epoch)
 
     # right now this is an overridden method by the inherited class...
     # do we want models to inherit experiment or for experiments to ingest models?
@@ -68,7 +73,16 @@ class Experiment(torch.nn.Module):
     #              2) New experiment subclass for each train step type (meh), idk
     #              3) new class
     def train_step(self, data, grad=True, output=False):
-
+        """
+        The output of train_step should be a dictionary containing losses to log.
+        Examples:
+                loss_dict = {
+                    "total_loss": L_mean,
+                    "representation_loss": L_rep_mean,
+                    "reconstruction_loss": L_rec_mean,
+                    "regularization_loss": L_reg_mean,
+                }
+        """
         raise NotImplementedError
 
     # do we want to pass in data and let the train loading scheme be passed in/parameterized in constructor?
@@ -83,27 +97,40 @@ class Experiment(torch.nn.Module):
                 self.optimizer.__dict__, self.logdir, "optimizer" + "_dict"
             )
             generic.save_pickle(
-                self.regularizer.__dict__, self.logdir, "regularizer" + "_dict"
+                data_loader.__dict__, self.logdir, "data_loader" + "_dict"
             )
             generic.save_pickle(
-                self.data_loader.__dict__, self.logdir, "data_loader" + "_dict"
-            )
-            generic.save_pickle(
-                self.data_loader.training_data.__dict__,
+                data_loader.train.__dict__,
                 self.logdir,
                 "training_data" + "_dict",
             )
+            if self.regularizer is not None:
+                generic.save_pickle(
+                    self.regularizer.__dict__, self.logdir, "regularizer" + "_dict"
+                )
 
         except:
             raise Exception("Problem creating logging and/or checkpoint directory.")
 
-        for i in range(start_epoch, epochs):
+        for i in tqdm(range(start_epoch, epochs)):
             training_loss = self.train_step(data_loader.train, grad=True)
-            self.log(i, group="loss", name="train", value=training_loss)
+            for k in training_loss.keys():
+                self.log(i, group=k, name="train", value=training_loss[k])
 
             if data_loader.val is not None:
                 validation_loss = self.train_step(data_loader.val, grad=False)
-                self.log(i, group="loss", name="val", value=validation_loss)
+                for k in validation_loss.keys():
+                    self.log(i, group=k, name="val", value=validation_loss[k])
+
+            self.print_update(training_loss, validation_loss, epoch)
+
+    def print_update(self, training_loss, validation_loss, epoch):
+        if epoch % self.print_frequency == 0:
+            print(
+                "Epoch {}  ||  Training Loss: {:0.5f}  |  Validation Loss: {:0.5f}".format(
+                    i, training_loss["total_loss"], validation_loss["total_loss"]
+                )
+            )
 
     def load_checkpoint(self, path, checkpoint):
         state_dict = torch.load(
