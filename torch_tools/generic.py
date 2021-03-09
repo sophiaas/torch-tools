e946b5b8f5ded6745ccfffbd8facd725528b4452
Sophia Sanborn <sophia@Sophias-MacBook-Pro.local>
2021-03-09 09:36:24 -0800

    fixes minor bugs

--- torch_tools/generic.py
+++ torch_tools/generic.py
@@ -4,11 +4,11 @@ import pickle
 
 class ParameterDict(dict):
     def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.__dict__ = self
 
 
 def save_pickle(param_dict, path, fname):
     final_path = os.path.join(path, fname)
     with open(final_path, "wb") as f:
-        pickle.dump(f, param_dict)
+        pickle.dump(param_dict, f)
